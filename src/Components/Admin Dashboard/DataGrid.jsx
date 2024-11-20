d1b56ae0622020f815f0ea9f5c0aa4be1923447d
Mohammad Abdullah <ma645431@gmail.com>
2024-11-20 16:26:58 +0500

    Added User Table for admin dashboard

--- src/Components/Admin Dashboard/DataGrid.jsx
+++ src/Components/Admin Dashboard/DataGrid.jsx
@@ -38,10 +38,10 @@ const AdminDashboard = () => {
     <div className="max-w-7xl mx-auto mt-10">
       <h1 className="text-2xl font-bold text-center mb-6">User Management</h1>
       <div className="overflow-x-auto">
-        <table className="min-w-full bg-white border border-gray-200">
+        <table className="min-w-full bg-white border border-gray-200 rounded-xl table-auto">
           <thead>
             <tr>
-              <th className="px-4 py-2 border-b border-gray-200 bg-gray-100 text-left">
+              <th className="px-4 py-2 border-b border-gray-200 bg-gray-100 text-left rounded-tl-xl">
                 #
               </th>
               <th className="px-4 py-2 border-b border-gray-200 bg-gray-100 text-left">
@@ -62,7 +62,7 @@ const AdminDashboard = () => {
               <th className="px-4 py-2 border-b border-gray-200 bg-gray-100 text-left">
                 Location
               </th>
-              <th className="px-4 py-2 border-b border-gray-200 bg-gray-100 text-left">
+              <th className="px-4 py-2 border-b border-gray-200 bg-gray-100 text-left rounded-tr-xl">
                 Skills
               </th>
             </tr>
